621ca8d9a001cac037780653a7903465418d085b
Loic GARDE <loic.garde@gmail.com>
2013-06-04 21:11:03 +0200

    Test

---
+++ tests/LoicGarde/OAuth/MyClassTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LoicGarde\OAuth ;
+
+class MyClassTest extends PHPUnit_Framework_TestCase {
+    
+     public function testAdd() {
+         $myClass = new MyClass ;
+         $a = 3 ;
+         $b = 4 ;
+         $c = $a + $b ;
+         $this->assertEquals($c,$myClass->add($a, $b)) ; 
+     } 
+}
+
+?>
