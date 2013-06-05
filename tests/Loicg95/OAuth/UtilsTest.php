06509578936ed087d973053ec7762d9dc1637032
Loic GARDE <loic.garde@gmail.com>
2013-06-05 22:27:21 +0200

    Test

--- tests/Loicg95/OAuth/UtilsTest.php
+++ tests/Loicg95/OAuth/UtilsTest.php
@@ -4,8 +4,12 @@ namespace Loicg95\OAuth ;
 
 class UtilsTest extends \PHPUnit_Framework_TestCase {
     
-     public function testUrlEncode() {
+     public function testUrlEncodeTilde() {
          $this->assertEquals("hello%25hola%20toto%7Eeee" ,Utils::urlencode("hello%hola toto~eee" )) ; 
+     } 
+     
+     public function testUrlEncode() {
+         $this->assertEquals("hello%25hola%20totoeee" ,Utils::urlencode("hello%hola totoeee" )) ; 
      } 
 }
 
