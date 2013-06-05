b6cf2a490e9c5281d8dcc2b5c5b49b5df8dfdad4
Loic GARDE <loic.garde@gmail.com>
2013-06-05 22:32:49 +0200

    Test

--- src/Loicg95/OAuth/Utils.php
+++ src/Loicg95/OAuth/Utils.php
@@ -8,7 +8,7 @@ class Utils {
     if (is_array($input)) {
         return array_map(array('Loicg95\OAuth\Utils', 'urlencode'), $input);
     } else if (is_scalar($input)) {
-        return str_replace('%7E', '~', rawurlencode($input));
+        return str_replace('~','%7E', rawurlencode($input));
     } else {
         return '';
     }
